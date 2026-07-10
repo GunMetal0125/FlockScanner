df5d32103b44aeaafff97bc68b5039cba1ae6b1c
GunMetal0125 <andy.arroyo@aol.com>
2026-07-10 02:35:54 -0400

    Update wifi.cpp

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -9,3 +9,18 @@ void setup() {
 
   displayMessage("Flock Detector Ready");
 }
+int n = WiFi.scanNetworks();
+
+for (int i = 0; i < n; i++) {
+    String mac = WiFi.BSSIDstr(i);
+
+    if (isFlockMAC(mac)) {
+        Serial.println("FLOCK CAMERA DETECTED: " + mac);
+
+        recordMAC(mac);  // your rolling hit counter
+
+        if (countRecentHits(5000) >= 3) {
+            triggerAlert();  // buzzer, LED, display, etc.
+        }
+    }
+}
