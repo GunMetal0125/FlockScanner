80173db85dff00d3cf5a8246e216bb9ba24f79e2
GunMetal0125 <andy.arroyo@aol.com>
2026-07-10 02:37:49 -0400

    Update wifi.cpp

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -30,3 +30,24 @@ String macToString(const uint8_t *mac) {
             mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     return String(buf);
 }
+struct Hit {
+    unsigned long time;
+    String mac;
+};
+
+std::vector<Hit> hits;
+
+void recordMAC(const String &mac) {
+    hits.push_back({millis(), mac});
+}
+
+int countRecentHits(unsigned long windowMs) {
+    unsigned long now = millis();
+    int count = 0;
+
+    for (auto &h : hits) {
+        if (now - h.time <= windowMs) count++;
+    }
+
+    return count;
+}
