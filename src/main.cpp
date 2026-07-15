8c84f6d405090f369f1dd96c3f4f060a32a29fd8
GunMetal0125 <andy.arroyo@aol.com>
2026-07-15 03:05:03 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <SPI.h>
 #include <SD.h>
 #include "LGFX_ILI9486.hpp"
 
@@ -27,54 +28,46 @@ void setup() {
   Serial.begin(115200);
   delay(200);
 
-  // -------------------------
-  //  LED
-  // -------------------------
+  // LED
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
 
-  // -------------------------
-  //  Display Init
-  // -------------------------
+  // Display Init
   tft.init();
   tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(BLACK);
 
-  // -------------------------
-  //  SD Card Init
-  // -------------------------
+  // SD Card Init
   if (!SD.begin()) {
     Serial.println("SD FAILED!");
-    tft.setTextColor(TFT_RED);
+    tft.fillScreen(BLACK);
+    tft.setTextColor(RED);
     tft.setTextSize(3);
     tft.setCursor(10, 10);
     tft.println("SD CARD ERROR");
     delay(2000);
-  }
-
-  // -------------------------
-  //  Boot Logo
-  // -------------------------
-  if (SD.exists("/badalien.jpg")) {
-    tft.drawJpgFile(SD, "/badalien.jpg", 0, 0);
   } else {
-    tft.setTextColor(TFT_GREEN);
-    tft.setTextSize(4);
-    tft.setCursor(20, 20);
-    tft.println("BAD ALIEN");
+    // Boot Logo
+    if (SD.exists("/badalien.jpg")) {
+      tft.drawJpgFile(SD, "/badalien.jpg", 0, 0);
+    } else {
+      tft.fillScreen(BLACK);
+      tft.setTextColor(GREEN);
+      tft.setTextSize(4);
+      tft.setCursor(20, 20);
+      tft.println("BAD ALIEN");
+    }
+    delay(2000);
   }
 
-  delay(2000);
-  tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(BLACK);
 
-  // -------------------------
-  //  WiFi Scanner Init
-  // -------------------------
+  // WiFi Scanner Init
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
 
-  tft.setTextColor(TFT_WHITE);
+  tft.setTextColor(WHITE);
   tft.setTextSize(2);
   tft.setCursor(10, 10);
   tft.println("Flock Detector Ready");
@@ -95,30 +88,25 @@ void loop() {
 
     for (int i = 0; i < n; i++) {
       String mac = WiFi.BSSIDstr(i);
-
       if (mac.startsWith(TARGET_MAC)) {
         flockDetected = true;
         break;
       }
     }
 
-    // -------------------------
-    //  Update Display + LED
-    // -------------------------
-    tft.fillScreen(TFT_BLACK);
+    // Update Display + LED
+    tft.fillScreen(BLACK);
     tft.setCursor(10, 10);
 
     if (flockDetected) {
-      tft.setTextColor(TFT_GREEN);
+      tft.setTextColor(GREEN);
       tft.setTextSize(3);
       tft.println("FLOCK CAMERA FOUND!");
-
       digitalWrite(LED_PIN, HIGH);
     } else {
-      tft.setTextColor(TFT_RED);
+      tft.setTextColor(RED);
       tft.setTextSize(3);
       tft.println("NO FLOCK NEARBY");
-
       digitalWrite(LED_PIN, LOW);
     }
   }
