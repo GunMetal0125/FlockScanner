01f500557181433b49f0e7257ffa9d31d41fb765
GunMetal0125 <andy.arroyo@aol.com>
2026-07-15 04:19:58 -0400

    Update display.h

--- include/display.h
+++ include/display.h
@@ -1,7 +1,10 @@
-#pragma once
-#include <Arduino.h>
-#include "gps.h"
+#ifndef DISPLAY_H
+#define DISPLAY_H
 
-void initDisplay();
-void displayMessage(String msg);
+#include <Arduino.h>
+#include "storage.h" // FIX: Includes the file that defines 'GPSData' so the compiler knows what it is
+
+// This function can now successfully compile because GPSData is defined!
 void displayGPS(GPSData d);
+
+#endif // DISPLAY_H
