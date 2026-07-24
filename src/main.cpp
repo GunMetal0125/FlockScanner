dd1edda07c48e5629bb6ce3e401479747cf7c2be
GunMetal0125 <andy.arroyo@aol.com>
2026-07-24 06:18:40 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
-#include "wifi_scanner.h"  // <--- ADD THIS LINE AT THE TOP
+#include "wifi_scanner.h"
 
 void setup() {
-    // ...
+    // Initialize hardware (e.g., Serial, Display, Wi-Fi)
+    Serial.begin(115200);
 }
 
 void loop() {
     processWifiScan();
+
     if (targetDetected) {
         logDetection(lastMAC, lastRSSI);
         updateDisplayUI(true, lastMAC, lastRSSI, currentChannel);
