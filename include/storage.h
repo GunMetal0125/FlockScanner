da08c9e82d0553be9d90011f48712e8a41702fec
GunMetal0125 <andy.arroyo@aol.com>
2026-07-24 03:05:28 -0400

    Update storage.h

--- include/storage.h
+++ include/storage.h
@@ -1,18 +1,5 @@
-#ifndef STORAGE_H
-#define STORAGE_H
-
-struct GPSData {
-    // We defined both short and long names so nothing breaks!
-    float lat;
-    float latitude;
-    
-    float lng;
-    float longitude;
-    
-    float altitude;
-};
+#pragma once
+#include <Arduino.h>
 
 void initStorage();
-void logData(GPSData d);
-
-#endif // STORAGE_H
+void logDetection(const char* mac, int rssi);
