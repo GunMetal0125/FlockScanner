d0b1aec39c9946a28d253817c9543582febc169a
GunMetal0125 <andy.arroyo@aol.com>
2026-07-24 06:17:08 -0400

    Update wifi.cpp

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -1,17 +1,17 @@
-#include <WiFi.h>
-#include "wifi_scanner.h"
+#ifndef WIFI_SCANNER_H
+#define WIFI_SCANNER_H
 
-// Actual variable definitions
-bool targetDetected = false;
-String lastMAC = "";
-int lastRSSI = 0;
-int currentChannel = 1;
+#include <Arduino.h>
 
-void scanWifiNetworks() {
-    int n = WiFi.scanNetworks();
-    // ...
-}
+// 1. Function Prototypes
+void processWifiScan();
+void updateDisplayUI(bool target, String mac, int rssi, int channel);
+void logDetection(String mac, int rssi);
 
-void processWifiScan() {
-    // ...
-}
+// 2. Variable Declarations (tells main.cpp these exist elsewhere)
+extern bool targetDetected;
+extern String lastMAC;
+extern int lastRSSI;
+extern int currentChannel;
+
+#endif
