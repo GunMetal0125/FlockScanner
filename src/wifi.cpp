1dfbf7c68ad56e3b595301249851e0e1164584f2
GunMetal0125 <andy.arroyo@aol.com>
2026-07-24 03:47:05 -0400

    Update wifi.cpp

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -1,2 +1,21 @@
-code ...
+#include <Arduino.h>
+#include <WiFi.h>
+
+void initWifiScanner() {
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+}
+
+void scanWifiNetworks() {
+    int n = WiFi.scanNetworks();
+    if (n == 0) {
+        // No networks found
+    } else {
+        for (int i = 0; i < n; ++i) {
+            // Print network details
+            WiFi.SSID(i);
+            WiFi.RSSI(i);
+        }
+    }
 }
